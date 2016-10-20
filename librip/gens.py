d2e4b7a134247a413c1d5d484258eac4be597391
IrinaMatveichuk <matveichuck.ira@yandex.ru>
2016-10-20 20:52:31 +0300

    Add files via upload

--- librip/gens.py
+++ librip/gens.py
@@ -1,6 +1,5 @@
 import random
 
-
 # Генератор вычленения полей из массива словарей
 # Пример:
 # goods = [
@@ -11,8 +10,22 @@ import random
 # field(goods, 'title', 'price') должен выдавать {'title': 'Ковер', 'price': 2000}, {'title': 'Диван для отдыха', 'price': 5300}
 
 def field(items, *args):
+    index = 0
     assert len(args) > 0
-    # Необходимо реализовать генератор 
+    if (len(args) == 1):
+        for el in items:
+            yield el.get(args[0])
+            index += 1
+    else:
+        for el in items:
+            d1 = {}
+            for key in args:
+                if (el.get(key) != None):
+                    d1[key] = el[key]
+                else:
+                    break
+            if (len(d1) != 0):
+                yield d1
 
 
 # Генератор списка случайных чисел
@@ -20,5 +33,6 @@ def field(items, *args):
 # gen_random(1, 3, 5) должен выдать примерно 2, 2, 3, 2, 1
 # Hint: реализация занимает 2 строки
 def gen_random(begin, end, num_count):
-    pass
+    for i in range(num_count):
+        yield random.randint(begin, end)
     # Необходимо реализовать генератор
--- librip/iterators.py
+++ librip/iterators.py
@@ -1,17 +1,42 @@
 # Итератор для удаления дубликатов
 class Unique(object):
+    i = -1
+    new_list = []
     def __init__(self, items, **kwargs):
-        # Нужно реализовать конструктор
-        # В качестве ключевого аргумента, конструктор должен принимать bool-параметр ignore_case,
-        # в зависимости от значения которого будут считаться одинаковые строки в разном регистре
         # Например: ignore_case = True, Aбв и АБВ разные строки
         #           ignore_case = False, Aбв и АБВ одинаковые строки, одна из них удалится
         # По-умолчанию ignore_case = False
-        pass
+        try:
+            self.ignore_case = kwargs['ignore_case']
+        except: self.ignore_case = False
+        self.items = items
+        self.index = 0
+        if (type(self.items) != list):
+            gen = self.items
+            self.items = []
+            for el in gen:
+                self.items.append(el)
+        self.index = len(self.items)
 
     def __next__(self):
-        # Нужно реализовать __next__    
-        pass
+        self.i += 1
+
+        if (self.i == self.index):
+            raise StopIteration
+
+        fl = False
+        if (self.ignore_case == False and type(self.items[self.i]) != int):
+            if (self.new_list.count(self.items[self.i].upper()) == 0 and self.new_list.count(self.items[self.i].lower()) == 0):
+                self.new_list.append(self.items[self.i])
+                self.fl = True
+                return self.items[self.i]
+            else: return self.__next__()
+        else:
+            if (self.new_list.count(self.items[self.i]) == 0):
+                self.new_list.append(self.items[self.i])
+                self.fl = True
+                return self.items[self.i]
+            else: return self.__next__()
 
     def __iter__(self):
         return self
