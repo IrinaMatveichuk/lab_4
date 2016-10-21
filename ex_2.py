37b5d559da19c0a67ccdee253903f218f504deab
IrinaMatveichuk <matveichuck.ira@yandex.ru>
2016-10-21 14:05:38 +0300

    Add files via upload

--- ex_2.py
+++ ex_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 from librip.gens import gen_random
-from librip.iterators_2 import Unique
+from librip.iterators import Unique
 
 data1 = [1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 'Aaa', 'aaa', 'aAa', 'Aaa', 'AAa', 'aAA', 'AAA', 'aaa']
 data2 = gen_random(1, 3, 10)
